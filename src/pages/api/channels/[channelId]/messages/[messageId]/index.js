fa7aaf3552d7c50db32a4644f901a515d889694f
SueLee <slee1008@my.bcit.ca>
2023-02-12 19:11:27 -0800

    edit get, put, delete for messageId

--- src/pages/api/channels/[channelId]/messages/[messageId]/index.js
+++ src/pages/api/channels/[channelId]/messages/[messageId]/index.js
@@ -6,21 +6,32 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
       // Get a message by ID
-      res
-        .status(200)
-        .json({ message: "GET request to /api/channels/[channelId]/messages/[messageId] not implemented yet" });
+      const message = await getMessageById(messageId);
+      if (!message) {
+        res.status(404).json({ message: "Message not found" });
+        break;
+      }
+      res.status(200).json(message);
       break;
     case "PUT":
       // Update a message by ID
-      res
-        .status(200)
-        .json({ message: "PUT request to /api/channels/[channelId]/messages/[messageId] not implemented yet" });
+      const { text } = req.body;
+      if (!text) {
+        res.status(400).json({ message: "Missing message text" });
+        break;
+      }
+      const updatedMessage = await updateMessageById(messageId, text);
+      if (!updatedMessage) {
+        res.status(404).json({ message: "Message not found" });
+        break;
+      }
+      res.status(200).json(updatedMessage);
       break;
     case "DELETE":
       // Delete a message by ID
+      await deleteMessageById(messageId);
       res
-        .status(200)
-        .json({ message: "DELETE request to /api/channels/[channelId]/messages/[messageId] not implemented yet" });
+        .status(204).end();
       break;
     default:
       res.status(405).end();
